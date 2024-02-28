9e7f9fa03e367cd9bff69ced1e01c99ae2e22273
stacybat <79266753+stacybat@users.noreply.github.com>
2024-02-28 18:12:09 +0300

    Update spi_device.py

--- Max31865/spi_device.py
+++ Max31865/spi_device.py
@@ -1,3 +1,5 @@
+
+
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 Scott Shawcroft for Adafruit Industries
@@ -19,10 +21,12 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# pylint: disable=too-few-public-methods"""
+# pylint: disable=too-few-public-methods
+"""
 SPI Bus Device
 ====================================================
-"""class SPIDevice:
+"""
+class SPIDevice:
     """
     Represents a single SPI device and manages locking the bus and the device
     address.:param ~busio.SPI spi: The SPI bus the device is on
@@ -51,13 +55,18 @@ SPI Bus Device
         self.baudrate = baudrate
         self.polarity = polarity
         self.phase = phase
-        self.extra_clocks = extra_clocksself.chip_select = chip_selectdef __enter__(self):
+        self.extra_clocks = extra_clocks
+        self.chip_select = chip_select
+    def __enter__(self):
         ## Locking has been disabled (for now) as MicroPython SPI object does not support it
         # while not self.spi.try_lock():
         #     pass
-        # self.spi.configure(baudrate=self.baudrate, polarity=self.polarity, phase=self.phase)self.chip_select.value(0)
-        return self.spidef __exit__(self, *exc):
-        self.chip_select.value(1)if self.extra_clocks > 0:
+        # self.spi.configure(baudrate=self.baudrate, polarity=self.polarity, phase=self.phase)
+        self.chip_select.value(0)
+        return self.spi
+    def __exit__(self, *exc):
+        self.chip_select.value(1)
+        if self.extra_clocks > 0:
             buf = bytearray(1)
             buf[0] = 0xff
             clocks = self.extra_clocks // 8
